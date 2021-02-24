6af214dbf9da3288b362a2529a75914e17ee7fa6
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2021-02-24 13:00:57 +0000

    q3

--- Week5DatabaseOOD/MainWindow.xaml.cs
+++ Week5DatabaseOOD/MainWindow.xaml.cs
@@ -31,5 +31,26 @@ namespace Week5DatabaseOOD
                         select c.CompanyName;
             lbxCustimersEX1.ItemsSource = query.ToList();
         }
+        private void btnQueryEx2_Click(object sender,RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c;
+            dgCustomersEX2.ItemsSource = query.ToList();
+        }
+        private void btnQueryEx3_Click(object sender,RoutedEventArgs e)
+        {
+            var query = from o in db.Orders
+                        where o.Customer.City.Equals("London")
+                        || o.Customer.City.Equals("Paris")
+                        || o.Customer.Country.Equals("USA")
+                        orderby o.Customer.ContactName
+                        select new
+                        {
+                            CustomerName = o.Customer.CompanyName,
+                            City = o.Customer.City,
+                            Address = o.ShipAddress
+                        };
+            dgCustomersEX3.ItemsSource = query.ToList().Distinct();
+        }
     }
 }
